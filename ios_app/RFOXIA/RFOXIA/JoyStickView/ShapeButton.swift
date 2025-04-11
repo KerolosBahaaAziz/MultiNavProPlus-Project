c867d98709ba7c8faab00d757343417b1983c4b8
youssab <youssabyasser@gmail.com>
2025-04-11 18:57:22 +0200

    Joy Stick Buttons Added

---
+++ ios_app/RFOXIA/RFOXIA/JoyStickView/ShapeButton.swift
@@ -0,0 +1,38 @@
+//
+//  ShapeButton.swift
+//  Testing JoyStick with SwiftUI
+//
+//  Created by Yasser Yasser on 11/04/2025.
+//
+
+import SwiftUI
+
+struct ShapeButton: View {
+    
+    let shape : ActionButtonsView.ShapeType
+    
+    var body: some View {
+        Image(systemName :iconName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 30, height: 30)
+            .foregroundColor(.white)
+            .padding()
+            .background(.purple)
+            .clipShape(Circle())
+    }
+    
+    
+    private var iconName : String {
+        switch(shape) {
+        case .circle : return "circle"
+        case .square : return "square"
+        case .triangle : return "triangle"
+        case .xshape : return "xmark"
+        }
+    }
+}
+
+#Preview {
+    ShapeButton(shape: .circle)
+}
