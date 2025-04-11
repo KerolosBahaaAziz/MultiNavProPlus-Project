ec656f57e489a0040bfd2901765e57dd49be29d4
kemad97 <kerolos.emad992@gmail.com>
2025-04-11 23:44:15 +0200

    add chat screen

---
+++ android_app/app/src/main/java/com/example/multinav/chat/ChatViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.multinav.chat
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+data class Message(val text: String, val isSentByUser: Boolean)
+
+class ChatViewModel : ViewModel() {
+
+    private val _messages = MutableStateFlow<List<Message>>(
+        listOf(
+            Message("Hello!", false),
+            Message("Hi there!", true)
+        )
+    )
+
+       val messages: StateFlow<List<Message>> = _messages
+
+    fun sendMessage(message: String) {
+        viewModelScope.launch {
+            _messages.value = _messages.value + Message(message, true)
+        }
+    }
+
+    fun receiveMessage(message: String) {
+        viewModelScope.launch {
+            _messages.value = _messages.value + Message(message, false)
+        }
+    }
+}
