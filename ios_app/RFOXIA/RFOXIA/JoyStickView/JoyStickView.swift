58d1b2ed39467969d22f92515c6e1e280d5d20a2
youssab <youssabyasser@gmail.com>
2025-04-11 19:54:02 +0200

    JoyStickView Added to test the joyStick View and Sensor Readings View Made

---
+++ ios_app/RFOXIA/RFOXIA/JoyStickView/JoyStickView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Testing JoyStick with SwiftUI
+//
+//  Created by Yasser Yasser on 11/04/2025.
+//
+
+import SwiftUI
+
+struct JoyStickView: View {
+    var body: some View {
+        VStack{
+            SensorsReadingView()
+            HStack {
+                DirectionPadView(onDirectionPressed: { direction in
+                    print("direction is \(direction)")
+                })
+                Spacer()
+                ActionButtonsView(onActionPressed: { Action in
+                    print("Action is \(Action)")
+                })
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    JoyStickView()
+}
