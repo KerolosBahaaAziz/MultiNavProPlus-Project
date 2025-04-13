cc3a760c0eb8891808160dd03923a9bba52304c1
KerolosBahaaAziz <kerolosbahaa6@gmail.com>
2025-04-13 11:00:04 +0200

    animitaion class

--- ios_app/RFOXIA/RFOXIA/ContentView.swift
+++ ios_app/RFOXIA/RFOXIA/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
 
     var body: some View {
         // Pass the managed object context to BluetoothChatView
-        CheckBluetoothView()
+        BluetoothChatView()
             .environment(\.managedObjectContext, viewContext)
     }
 }
