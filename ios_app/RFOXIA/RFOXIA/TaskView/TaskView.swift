36dbda5b24bb4f1f00ea4c4c7b689062410d72af
youssab <youssabyasser@gmail.com>
2025-04-14 22:37:38 +0200

    Colors for buttons has been changed

---
+++ ios_app/RFOXIA/RFOXIA/TaskView/TaskView.swift
@@ -0,0 +1,19 @@
+//
+//  TaskView.swift
+//  RFOXIA
+//
+//  Created by Yasser Yasser on 14/04/2025.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    
+    var body: some View {
+        Text("asdasd")
+    }
+}
+
+#Preview {
+    TaskView()
+}
