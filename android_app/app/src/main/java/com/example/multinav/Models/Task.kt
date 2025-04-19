b991062627e65774958ea95792be4f8e422c40f4
GeorgeSamir99 <georgesamirayead@gmail.com>
2025-04-19 14:58:32 +0200

    +make Task Model

---
+++ android_app/app/src/main/java/com/example/multinav/Models/Task.kt
@@ -0,0 +1,21 @@
+package com.example.multinav.Models
+
+data class Task(
+    val title :String,
+    val isTurnOn:Boolean = false,
+
+    val triangle: String = "Triangle",
+    val circle: String = "circle",
+    val square: String = "square",
+    val x: String = "x",
+
+    val arrowUP: String = "UP",
+    val arrowDown: String = "Down",
+    val arrowLeft: String = "Left",
+    val arrowRight: String = "Right",
+
+    val buttonA: Char = 'A',
+    val buttonB: Char = 'B',
+    val buttonC: Char = 'C',
+    val buttonD: Char = 'D',
+    )
