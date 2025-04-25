2b950f56d9d1c4476c9c3710f4d84af4ae5572c0
kemad97 <kerolos.emad992@gmail.com>
2025-04-26 00:57:00 +0300

    Update navigation.kt

--- android_app/app/src/main/java/com/example/multinav/navigation.kt
+++ android_app/app/src/main/java/com/example/multinav/navigation.kt
@@ -1,5 +1,4 @@
 package com.example.multinav
-import ChatScreen
 import JoyStickScreen
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -9,6 +8,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.multinav.chat.ChatScreen
 import com.example.multinav.chat.ChatViewModel
 import com.example.multinav.chat.ChatViewModelFactory
 import com.example.multinav.main_screen.MainScreen
